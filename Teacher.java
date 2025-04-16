a31f8494ce285f4de9a4dcea7d34d5bf936b447c
jeffreyThe3155 <jjl28282@gmail.com>
2025-04-16 20:47:47 +0000

    commit

--- Teacher.java
+++ Teacher.java
@@ -1,13 +1,33 @@
 public class Teacher extends Person{
-    
+    private String mySubject;
+    private int mySalary;
     // TODO: Add fields.
 
-    public Teacher(String name, int age, String gender)
+    public Teacher(String name, int age, String gender, String subject, int salary)
     {
         super(name, age, gender);
+
+        mySubject = subject;
+        mySalary = salary;
     }
 
     // TODO Add methods
+
+    public String getSubject() {
+        return mySubject;
+    }
+
+    public String getSalary() {
+        return mySalary;
+    }
+
+    public void setSubject(String subject) {
+        mySubject = subject;
+    }
+
+    public void setSalary(int salary) {
+        mySalary = salary;
+    }
 
     public String toString(){
         // TODO: improve this.
